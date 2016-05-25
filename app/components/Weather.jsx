2880da32dc104e016c8da64ebe7580e22664bccf
kickinbahk <kickinbahk@gmail.com>
2016-05-24 18:22:46 -0700

    add get initial state

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -3,8 +3,17 @@ var WeatherForm = require('WeatherForm')
 var WeatherMessage = require('WeatherMessage')
 
 var Weather = React.createClass({
+  getInitialState: function () {
+    return {
+      location: 'Miami',
+      temp: 88
+    }
+  },
   handleSearch: function (location) {
-    window.alert(location)
+    this.setState({
+      location: location,
+      temp: 23
+    })
   },
   render: function () {
     return (
