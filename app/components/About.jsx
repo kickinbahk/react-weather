1a417ca591a662460fd1b22993b0c134a95a6a73
kickinbahk <kickinbahk@gmail.com>
2016-06-29 08:15:45 -0700

    add selectors to update styles on about and examples pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react')
 var About = (props) => {
   return (
     <div>
-      <h1 className='text-center'>About</h1>
+      <h1 className='text-center page-title'>About</h1>
       <p>Welcome to the React Weather app!</p>
       <p>It will allow you to get the weather of any location you would like!</p>
       <p>This is built using <a href='https://facebook.github.io/react/'>React</a> and <a href='http://openweathermap.org/api'>Open Weather API</a>.</p>
