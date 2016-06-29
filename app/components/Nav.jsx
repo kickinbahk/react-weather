0c573347bfcffe6cbb74129b7e35ff53a2a967af
kickinbahk <kickinbahk@gmail.com>
2016-06-28 17:34:26 -0700

    update nav styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,11 +3,24 @@ var {IndexLink} = require('react-router')
 
 var Nav = (props) => {
   return (
-    <div>
-      <h2>Nav Component</h2>
-      <IndexLink to='/'activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
-      <IndexLink to='/examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+    <div className='top-bar'>
+      <div className='top-bar-left'>
+        <ul className='menu'>
+          <li className='menu-text'>React Weather App</li>
+          <li>
+            <IndexLink to='/'activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+          </li>
+          <li>
+            <IndexLink to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+          </li>
+          <li>
+            <IndexLink to='/examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className='top-bar-right'>
+
+      </div>
     </div>
   )
 }
