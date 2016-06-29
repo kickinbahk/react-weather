48dcfabfe2304c1e2767eae09b1712527c1f0f38
kickinbahk <kickinbahk@gmail.com>
2016-06-29 07:23:17 -0700

    add function to show modal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,7 +1,10 @@
 var React = require('react')
 
 var ErrorModal = React.createClass({
-
+  componentDidMount: function () {
+    var modal = new Foundation.Reveal($('#error-modal'))
+    modal.open()
+  },
   render: function () {
     return (
       <div id='error-modal' className='reveal tiny text-center' data-reveal>
