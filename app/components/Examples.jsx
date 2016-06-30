9bc8894b9936f2f4524a70f31ee349576961b39e
kickinbahk <kickinbahk@gmail.com>
2016-06-30 07:54:49 -0700

    update examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -8,7 +8,7 @@ var Examples = (props) => {
       <p>Here are a few example locations to try out:</p>
       <ol>
         <li>
-          <Link to='/?location=Burbank'>Burbank, CA</Link>
+          <Link to='/?location=Los Angeles'>Los Angeles, CA</Link>
         </li>
         <li>
           <Link to='/?location=London'>London, England</Link>
