42bd966de8bbae40502ba6728dfba1ca6c476dbc
kickinbahk <kickinbahk@gmail.com>
2016-06-30 07:53:30 -0700

    wire up search and update weather component when searching

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var {IndexLink} = require('react-router')
 var Nav = React.createClass({
   onSearch: function (e) {
     e.preventDefault()
-    window.alert('Not yet wired up')
+    var location = this.refs.search.value
+    var encodedLocation = encodeURIComponent(location)
+
+    if (location.length > 0) {
+      this.refs.search.value = ''
+      window.location.hash = `#/?location=${encodedLocation}`
+    }
   },
   render: function () {
     return (
@@ -26,8 +32,12 @@ var Nav = React.createClass({
         <div className='top-bar-right'>
           <form onSubmit={this.onSearch}>
             <ul className='menu'>
-              <li><input type='search' placeholder='Search Weather by City' /></li>
-              <li><input type='submit' className='button' value='Get Weather' /></li>
+              <li>
+                <input type='search' placeholder='Search Weather by City' ref='search' />
+              </li>
+              <li>
+                <input type='submit' className='button' value='Get Weather' />
+              </li>
             </ul>
           </form>
         </div>
